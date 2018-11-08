3d43ae1fcb03c648541045407c31e7fcc16715b4
simondr <simodraxl@tsn.at>
2018-11-08 16:07:41 +0100

    Draxl Changes

---
+++ Spiel/Bogenschießen/Assets/Script/ball_movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_movement : MonoBehaviour {
+
+
+    public Rigidbody Ball;
+	// Use this for initialization
+	void Start () {
+
+        Ball.useGravity = true;
+        Ball.AddForce(0, 100, 200)
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+
+       
+
+
+		
+	}
+}
